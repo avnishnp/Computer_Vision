6c2a8ef13b7805588d10671a79e07ad49f90bdd0
avnishnp <avnishnp07@gmail.com>
2023-08-23 14:51:01 -0400

    requirements added

---
+++ YoloV8-Cell-Segmentation/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'Avnish Patel',
+    author_email= 'avnp07@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
