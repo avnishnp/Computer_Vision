298e51a2e772c42ca502af2062ad44c62c2a3f0c
avnishnp <avnishnp07@gmail.com>
2023-08-24 14:02:37 -0400

    data validation added

--- YoloV8-Cell-Segmentation/cellSegmentation/constant/training_pipeline/__init__.py
+++ YoloV8-Cell-Segmentation/cellSegmentation/constant/training_pipeline/__init__.py
@@ -11,3 +11,13 @@ DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1VWFeRxyGq-bBS5AIuJBtC
 
 # the working would be like artifacts/data_ingestion/data.zip 
 # then artifacts/feature_store/train,valid,data.yaml
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["train", "valid", "test", "data.yaml"]
